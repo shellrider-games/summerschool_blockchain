a11f953b3e36b789bb0534669582896842c28393
Georg Becker <georg.becker@shellrider.com>
2025-06-24 09:38:50 +0200

    Adds calculateHash function to Block

--- shellriderCoin.js
+++ shellriderCoin.js
@@ -1,3 +1,5 @@
+const { createHash} = require('crypto');
+
 class Block {
     constructor(index, timestamp, data, previousHash = '') {
         this.inex = index;
@@ -5,5 +7,14 @@ class Block {
         this.timestamp = timestamp;
         this.data = data;
         this.hash = '';
+    }
+
+    calculateHash() {
+        return createHash('sha256').update(
+            this.index +
+            this.previousHash +
+            this.timestamp +
+            JSON.stringify(this.data)
+        ).digest('hex');
     }
 }
