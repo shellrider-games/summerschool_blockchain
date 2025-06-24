7b0ad0b8da506e6d42512a8341b7f4df06f4619f
Georg Becker <georg.becker@shellrider.com>
2025-06-24 11:31:31 +0200

    Implements basic mining

--- shellriderCoin.js
+++ shellriderCoin.js
@@ -6,6 +6,7 @@ class Block {
         this.previousHash = previousHash;
         this.timestamp = timestamp;
         this.data = data;
+        this.nonce = 0;
         this.hash = this.calculateHash();
     }
 
@@ -14,14 +15,24 @@ class Block {
             this.index +
             this.previousHash +
             this.timestamp +
-            JSON.stringify(this.data)
+            JSON.stringify(this.data) +
+            this.nonce
         ).digest('hex');
+    }
+
+    mine(difficulty) {
+        while (this.hash.substring(0,difficulty) !== Array(difficulty + 1).join('0')){
+            this.nonce++;
+            this.hash = this.calculateHash();
+        }
+        console.log(`Block minded: ${this.hash} in ${this.nonce} iterations`);
     }
 }
 
 class Blockchain {
     constructor() {
         this.chain = [this.createGenesisBlock()];
+        this.difficulty = 5;
     }
 
     createGenesisBlock() {
@@ -34,7 +45,7 @@ class Blockchain {
 
     addBlock(nextBlock) {
         nextBlock.previousHash = this.getLatestBlock().hash;
-        nextBlock.hash = nextBlock.calculateHash();
+        nextBlock.mine(this.difficulty);
         this.chain.push(nextBlock);
     }
 
@@ -48,6 +59,7 @@ class Blockchain {
         }
         return true;
     }
+
 }
 
 let shellriderCoin = new Blockchain();
@@ -55,9 +67,5 @@ shellriderCoin.addBlock(new Block(1, "24.06.2025", { amount : 5 }));
 shellriderCoin.addBlock(new Block(2, "24.06.2025", { amount : 8 }));
 shellriderCoin.addBlock(new Block(3, "24.06.2025", { amount : 13 }));
 
-console.log(JSON.stringify(shellriderCoin, null, 4));
-console.log("Chain is valid?", JSON.stringify(shellriderCoin.isValid()));
-shellriderCoin.chain[2].data = { amount : 500 };
-shellriderCoin.chain[2].hash = shellriderCoin.chain[2].calculateHash();
 console.log(JSON.stringify(shellriderCoin, null, 4));
 console.log("Chain is valid?", JSON.stringify(shellriderCoin.isValid()));
